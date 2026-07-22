06e3ba375268d5906f8b4c238ba9376bd3219c7d
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-07-23 00:16:14 +0800

    feat: 构建时复制&合并库文件

--- MSLXPluginEntry.cs
+++ MSLXPluginEntry.cs
@@ -23,31 +23,34 @@ public class MSLXPluginEntry : IPlugin
     public async void OnPluginInitialize(IServiceProvider serviceProvider)
     {
         Instance = this;
-        
+    }
+
+    public async void OnLoad()
+    {
         SDK.MSLX.Logger.Info("mslx-plugin-demo 载入成功~");
         SDK.MSLX.Logger.Info("当前存在实例数量：" + SDK.MSLX.Config.Servers.GetServerList().Count.ToString());
-        
+
         // ===== 配置读写示例 =====
         string dataDir = MSLXPluginEntry.Instance.Config().GetDataPath();
-        SDK.MSLX.Logger.Info("使用的数据目录："+dataDir);
-        
+        SDK.MSLX.Logger.Info("使用的数据目录：" + dataDir);
+
         MSLXPluginEntry.Instance.Config().WriteConfigKey("author", "xiaoyu");
         MSLXPluginEntry.Instance.Config().WriteConfigKey("magicNumber", 1027);
-        
+
         int count = (int?)MSLXPluginEntry.Instance.Config().ReadConfigKey("magicNumber") ?? 0;
         SDK.MSLX.Logger.Info("从配置文件读取magicNumber：" + count.ToString());
-        
+
         var allConfig = MSLXPluginEntry.Instance.Config().ReadConfig();
-        
-        
+
+
         // get请求示例
         var response = await SDK.MSLX.Http.GetAsync("https://api.mslmc.cn/v3/query/notice?query=id");
-        
+
         if (response.IsSuccessStatusCode)
         {
             JObject jobj = JObject.Parse(response.Content ?? "{}");
             string content = jobj["data"]?["noticeID"]?.ToString() ?? "";
-            
+
             SDK.MSLX.Logger.Info($"获取到的MSL公告编号: {content}");
         }
 
@@ -58,8 +61,8 @@ public class MSLXPluginEntry : IPlugin
             PluginHttpContentType.Json,
             new { username = "admin", action = "start" }
         ); ***/
-        
-        
+
+
         // ===== 下载器调用示例 ===== 
         SDK.MSLX.Logger.Info("准备下载文件...");
         string targetPath = Path.Combine(MSLXPluginEntry.Instance.Config().GetDataPath(), "server.jar");
@@ -69,11 +72,11 @@ public class MSLXPluginEntry : IPlugin
         }
 
         var result = await SDK.MSLX.Downloader.DownloadFileAsync(
-            "https://bmclapi2.bangbang93.com/forge/download?mcversion=26.1.2&version=64.0.8&category=installer&format=jar", 
+            "https://bmclapi2.bangbang93.com/forge/download?mcversion=26.1.2&version=64.0.8&category=installer&format=jar",
             targetPath,
-            (progress, speed) => 
+            (progress, speed) =>
             {
-                SDK.MSLX.Logger.Debug($"\r下载中: {progress:0.0}% [{speed}]"); 
+                SDK.MSLX.Logger.Debug($"\r下载中: {progress:0.0}% [{speed}]");
             });
 
         if (result.Success)
@@ -85,6 +88,7 @@ public class MSLXPluginEntry : IPlugin
             SDK.MSLX.Logger.Error($"下载失败: {result.ErrorMessage}");
         }
     }
+
 
     public void OnUnload() {
         SDK.MSLX.Logger.Info("mslx-plugin-demo 卸载成功~");
